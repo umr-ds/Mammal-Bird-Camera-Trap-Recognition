d4517d7968c4ed4bfc20f459c74980e044ebf841
Daniel Schneider <schneider@mathematik.uni-marburg.de>
2023-09-15 11:52:49 +0000

    Added model and dataset download links

--- data/data_download.sh
+++ data/data_download.sh
@@ -3,10 +3,10 @@ dataset_name="MOF"
 
 if [ ! -d ${dataset_name} ]; then
     echo "Downloading ${dataset_name} data set..."
-    #wget -O  ${dataset_name}.tar https://data.uni-marburg.de/bitstream/handle/dataumr/XXX/data_${dataset_name}.tar
+    wget -O  ${dataset_name}.tar https://data.uni-marburg.de/bitstream/handle/dataumr/246/data_${dataset_name}.tar
     echo "Extracting..."
-    #mkdir ./${dataset_name}
-    #tar -xf ${dataset_name}.tar --directory ./${dataset_name}  # && rm ${dataset_name}.tar
+    mkdir ./${dataset_name}
+    tar -xf ${dataset_name}.tar --directory ./${dataset_name} && rm ${dataset_name}.tar
     echo "Done."
 else
     echo "${dataset_name} data set already downloaded..."
--- models/model_download.sh
+++ models/model_download.sh
@@ -3,10 +3,10 @@ model_name=$1
 
 if [ ! -d ${model_name} ]; then
     echo "Downloading ${model_name} model..."
-    #wget -O  ${model_name}.tar https://data.uni-marburg.de/bitstream/handle/dataumr/XXX/model_${model_name}.tar
+    wget -O  ${model_name}.tar https://data.uni-marburg.de/bitstream/handle/dataumr/246/model_${model_name}.tar
     echo "Extracting..."
-    #mkdir ./${model_name}
-    #tar -xf ${model_name}.tar --directory ./${model_name}  # && rm ${model_name}.tar
+    mkdir ./${model_name}
+    tar -xf ${model_name}.tar --directory ./${model_name} && rm ${model_name}.tar
     echo "Done."
 else
     echo "${model_name} already downloaded..."
