0ac4a79a2a26af8a7603a1b5e65af3679fa2dd74
Daniel Schneider <schneider@mathematik.uni-marburg.de>
2025-07-10 16:16:40 +0000

    Fixed download links. Added requirements file

--- data/data_download.sh
+++ data/data_download.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 dataset_names=$1
 
+declare -A download_links=( 
+["data_BNP"]="67650499-00dd-44cd-a7ad-e35764652984" 
+["data_MOF"]="05f14d67-496a-45e1-b66f-d9e24c748656"
+)
+
 for dataset_name in $dataset_names; do
     if [ ! -d ${dataset_name} ]; then
         echo "Downloading ${dataset_name} data set..." &&
-        wget -O  ${dataset_name}.tar https://data.uni-marburg.de/bitstream/handle/dataumr/556/data_${dataset_name}.tar &&
+        wget -O  ${dataset_name}.tar "https://data.uni-marburg.de/bitstreams/${download_links[${dataset_name}]}/download" &&
 
         echo "Extracting..." &&
         mkdir ./${dataset_name} &&
--- models/model_download.sh
+++ models/model_download.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 model_names=$1
 
+declare -A download_links=( 
+["model2023_ConvNextBase"]="a9c222be-35ef-46bd-996b-40c2363914bf" 
+["model2023_EfficientNetV2"]="ab841406-d791-4cd0-b8f5-eb8425d4ee3c"
+["model2024_ConvNextBase_species"]="d1455a3c-feee-4a7a-a739-eeea349ed3c6"
+["model2024_ConvNextBase_taxonomy"]="793fd538-021f-4e44-b9b0-666833c03c12"
+)
+
 for model_name in $model_names; do
-    if [ ! -d ${model_name} ]; then
+    if [[ ! -d ${model_name} ]]; then
         mkdir -p ./$(dirname ${model_name}) &&
         
         echo "Downloading ${model_name} model..." &&
-        wget -O  ${model_name}.tar https://data.uni-marburg.de/bitstream/handle/dataumr/556/${model_name/'/'/'_'}.tar &&
+        wget -O  ${model_name}.tar "https://data.uni-marburg.de/bitstreams/${download_links[${model_name/'/'/'_'}]}/download" &&
         
         echo "Extracting..." &&
         mkdir ${model_name} &&
